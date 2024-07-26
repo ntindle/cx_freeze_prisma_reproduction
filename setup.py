1ba22dd7e4c3cbc7d32fc5118e030aa2d631b3a7
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-07-26 01:37:23 -0300

    add prisma binary in the build

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ import os
 
 from cx_Freeze import Executable, setup  # type: ignore
 
+import prisma
+
 GUID = "{323a1ee5-317d-4423-8490-effd8f5bedcf}"
 
 packages = [
@@ -13,7 +15,10 @@ packages = [
     if m.ispkg and m.module_finder and "poetry" in m.module_finder.path  # type: ignore
 ]
 
-include_files = []  # source, destination in the bundle
+# source, destination in the bundle
+include_files = [
+    (prisma.BINARY_PATHS.query_engine["windows"], "prisma-query-engine-windows.exe")
+]
 
 # add the prisma directory if it exists
 if os.path.exists("./prisma"):
