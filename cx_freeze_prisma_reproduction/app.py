f4c68623fbf789af78911712127b19415056bb1a
Marcelo Duarte <marcelotduarte@users.noreply.github.com>
2024-08-03 21:13:20 -0300

    set PRISMA_BINARY_CACHE_DIR

--- cx_freeze_prisma_reproduction/app.py
+++ cx_freeze_prisma_reproduction/app.py
@@ -1,4 +1,6 @@
 import asyncio
+import os
+import sys
 from multiprocessing import freeze_support, set_start_method
 
 from prisma import Prisma
@@ -8,6 +10,8 @@ async def main():
     set_start_method("spawn", force=True)
     freeze_support()
 
+    if getattr(sys, "frozen", False):
+        os.environ["PRISMA_BINARY_CACHE_DIR"] = os.path.dirname(sys.executable)
     prisma = Prisma(auto_register=True)
 
     if not prisma.is_connected():
