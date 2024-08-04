0b0496c437772b48ba2a291edcd06fab035dfe6d
Nicholas Tindle <nick@ntindle.com>
2024-08-03 22:34:14 -0500

    try copying over node

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+import glob
 from pkgutil import iter_modules
 
 import os
+import sys
 
 from cx_Freeze import Executable, setup  # type: ignore
 
@@ -22,12 +24,38 @@ include_files = [
     (prisma.BINARY_PATHS.query_engine["windows"], "prisma-query-engine-windows.exe")
 ]
 
+
+def find_latest_node_folder(base_path):
+    pattern = os.path.join(base_path, "node-v*")
+    matching_folders = glob.glob(pattern)
+    if matching_folders:
+        return max(matching_folders, key=os.path.getctime)
+    return None
+
+
+# Determine the base path
+if sys.platform.startswith("win"):
+    base_path = os.path.expanduser("~/.cache/prisma-python/nodeenv/src")
+else:
+    # For non-Windows platforms, adjust the path accordingly
+    base_path = os.path.expanduser("~/.cache/prisma-python/nodeenv/src")
+
+# Find the latest node folder
+node_folder = find_latest_node_folder(base_path)
+
+if node_folder and os.path.exists(node_folder):
+    include_files.append((node_folder, "prisma-nodeenv"))
+else:
+    raise Exception("Node folder not found")
+
+
 # add the prisma directory if it exists
 if os.path.exists("./prisma"):
     include_files.append(("./prisma", "prisma"))
 
 if os.path.exists("./database.db"):
     include_files.append(("./database.db", "database.db"))
+
 
 # copy the prisma schema into the folder /prisma/schema.prisma
 if os.path.exists("./schema.prisma"):
